3f8683b9af45637badb09c8fef921de071180f3e
SergioF <fs220381@gmail.com>
2020-03-31 14:31:15 +0300

    + Solution class + ConvertToTitle method

---
+++ ExcelSheetColumnTitle/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelSheetColumnTitle
+{
+    class Solution
+    {
+        public static string ConvertToTitle(int n)
+        {
+            return string.Empty;
+        }
+    }
+}
